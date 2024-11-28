526443c1f4db86157156e289bc5fc18674ca734b
PAVIPRAVEENKUMAR <146061398+PAVIPRAVEENKUMAR@users.noreply.github.com>
2024-11-28 12:51:56 +0530

    Delete src/modules/src directory

--- src/modules/src/modules/shared/interfaces/claim.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface claim {
-    action: string; 
-    subject: string; 
-    conditions?: Record<string, any>;
-  }
--- src/modules/src/modules/users/users.guard.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { CaslBaseGuard } from '../authz/casl.base.guard';
-@Injectable()
-export class UsersGuard extends CaslBaseGuard {
-  protected getModuleName(): string {
-    return 'User';
-  }
-}
